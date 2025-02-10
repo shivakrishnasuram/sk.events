d4b66f81293d1d0d5963b07401b248cda0113705
shivakrishnasuram <suramshivakrishnasuram@gmail.com>
2025-02-11 01:07:29 +0530

    loading is in progress and nav bar is 50% completed

--- auth.js
+++ auth.js
@@ -32,12 +32,15 @@ signupform.addEventListener("submit", (event) => {
         .then((x) => {
             console.log(x)
             if (x.user) {
+
+                setTimeout(()=>{
+                    location.assign("loadings.html")
+                },2000)
                 alert("okay its nice ")
                 signinform.style.display = "inline-block"
                 signupform.style.display = "none"
                 btn.style.display = "none"
                 regist.style.display = "block"
-
             }
 
         }).catch((e) => {
